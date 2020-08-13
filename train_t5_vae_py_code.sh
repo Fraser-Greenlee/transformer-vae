5863720d6b9cd4ec08b33be768838d68cd8a5f73
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-08-13 08:15:19 +0000

    update training script

--- train_t5_vae_py_code.sh
+++ train_t5_vae_py_code.sh
@@ -1,11 +1,11 @@
 # Train an auto-encoder to compress code strings.
 
-export TRAIN_FILE=python_state_changes.txt
+export TRAIN_FILE=python_assign.txt
 # Use MODEL_PATH to load a previous run
 export MODEL_PATH=
 
-export MODEL_NAME=python_state_changes
-#export MODEL_PATH=python_state_changes
+export MODEL_NAME=python_assignments
+# export MODEL_PATH=python_assignments/checkpoint-3250
 export T5_MODEL_NAME=t5-large
 
 python t5_vae.py \
@@ -15,14 +15,12 @@ python t5_vae.py \
     --t5_model_name=t5-large \
     --do_train \
     --train_data_file=$TRAIN_FILE \
-    --per_device_train_batch_size 5 \
-    --gradient_accumulation_steps 16 \
+    --per_device_train_batch_size 28 \
+    --gradient_accumulation_steps 7 \
     --save_total_limit 1 \
     --save_steps 625 \
     --num_train_epochs 1 \
     --logging_steps 10 \
     --overwrite_output_dir \
     --ae_latent_size 1000 \
-    --set_seq_size 80 \
-
-# needs an effective batch size >200
+    --set_seq_size 20 \
