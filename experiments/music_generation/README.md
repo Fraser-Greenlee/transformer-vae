3f8b800356f2fa141dd02521cedbd86919f6ef59
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2020-11-19 15:08:48 +0000

    ready alternative tokenizer

---
+++ experiments/music_generation/README.md
@@ -0,0 +1,3 @@
+# [LakhNES](https://chrisdonahue.com/LakhNES/) music generation.
+
+Generate your own NES music by interpolating between samples in a latent space!
---
+++ experiments/music_generation/make_tokenizer.py
@@ -0,0 +1,18 @@
+from tokenizers import Tokenizer
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.models import WordLevel
+
+VOCAB_FILE = 'data/tx1_vocab.txt'
+
+with open(VOCAB_FILE, 'r') as f:
+    words = list(set(f.read().strip().split('\n')))
+
+vocab = {}
+for i, word in enumerate(['<pad>', '<unk>'] + words):
+    vocab[word] = i
+
+tokenizer = Tokenizer(WordLevel(vocab, unk_token='<unk>'))
+tokenizer.enable_padding(pad_token='<pad>')
+tokenizer.pre_tokenizer = Whitespace()
+
+tokenizer.save("data/tokenizer-LakhNES-tx1.json")
