721e43a78c201709928bea73b92b8ec8b591a655
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2020-11-28 14:57:34 +0000

    1st token encoding

---
+++ t5-1st-token.sh
@@ -0,0 +1,18 @@
+# Originally ran on 16GiB GPU
+WANDB_PROJECT="transformer-vae-tests" WANDB_WATCH=false python -c "from transformer_vae.train import main; main()" \
+    --output_dir=output \
+    --run_name="news t5 1st token" \
+    --do_train \
+    --do_eval \
+    --dataset_name="Fraser/news-category-dataset" \
+    --text_column=headline \
+    --per_device_train_batch_size 10 \
+    --gradient_accumulation_steps 3 \
+    --n_previous_latent_codes 0 \
+    --transformer_type funnel-t5 \
+    --set_seq_size 40 \
+    --logging_steps 100 \
+    --overwrite_output_dir \
+    --save_total_limit 3 \
+    --save_steps 1000 \
+    --mlm_probability 0 \
