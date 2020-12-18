86e5614710ab65525b53a33d5ebb8487ad012037
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2020-12-18 08:37:00 +0000

    Release: 0.0.2

--- transformer_vae/__init__.py
+++ transformer_vae/__init__.py
@@ -2,5 +2,5 @@ import collections
 
 __VersionInfo = collections.namedtuple("VersionInfo", ("major", "minor", "micro"))
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __version_info__ = __VersionInfo(*(map(int, __version__.split("."))))
