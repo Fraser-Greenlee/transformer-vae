1a53be8d9d5bec1ea13ed86d0237355fe71dc3f9
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2021-02-03 14:49:36 +0000

    remove SortishSampler

--- transformer_vae/trainer.py
+++ transformer_vae/trainer.py
@@ -119,38 +119,6 @@ class VAE_Trainer(trainer_script.Trainer):
                 num_training_steps=num_training_steps,
             )
 
-    def _get_train_sampler(self) -> Optional[torch.utils.data.sampler.Sampler]:
-        if isinstance(self.train_dataset, torch.utils.data.IterableDataset) or not isinstance(
-            self.train_dataset, collections.abc.Sized
-        ):
-            return None
-        src_lens = []
-        for row in tqdm(self.train_dataset, desc='Calculating dataset item lengths.'):
-            src_lens.append(len(row['input_ids']) - row['input_ids'].count(0))
-        return SortishSampler(src_lens)
-
-    def get_train_dataloader(self) -> DataLoader:
-        """
-        Returns the training :class:`~torch.utils.data.DataLoader`.
-
-        Will use no sampler if :obj:`self.train_dataset` does not implement :obj:`__len__`, a random sampler (adapted
-        to distributed training if necessary) otherwise.
-
-        Subclass and override this method if you want to inject some custom behavior.
-        """
-        if self.train_dataset is None:
-            raise ValueError("Trainer: training requires a train_dataset.")
-        train_sampler = self._get_train_sampler()
-
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.args.train_batch_size,
-            sampler=train_sampler,
-            collate_fn=self.data_collator,
-            drop_last=self.args.dataloader_drop_last,
-            num_workers=self.args.dataloader_num_workers,
-        )
-
     def _tokens_from_latent(self, latent):
         with torch.no_grad():
             old = self.model.config.use_extra_logs
